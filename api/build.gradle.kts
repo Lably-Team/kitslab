6795e153b46072326571bb25dc150a4c30ea7852
jonakls <asmot54@hotmail.com>
2022-04-14 17:56:00 -0500

    build(api): added Spigot and BungeeCord chat dependencies to project.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,13 +6,11 @@ group = "com.github.lablyteam"
 
 repositories {
     mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-chat:1.8-SNAPSHOT")
 }

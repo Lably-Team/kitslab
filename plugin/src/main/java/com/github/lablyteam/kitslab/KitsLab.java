c9e2348d662889945ac3996ff0c5348ac3208d93
jonakls <asmot54@hotmail.com>
2022-04-14 17:58:26 -0500

    feat(plugin): added binder modules of injectors and added injectors in general class

--- plugin/src/main/java/com/github/lablyteam/kitslab/KitsLab.java
+++ plugin/src/main/java/com/github/lablyteam/kitslab/KitsLab.java
@@ -1,16 +1,52 @@
 package com.github.lablyteam.kitslab;
 
+import com.github.lablyteam.kitslab.commands.KitsLabCommand;
+import com.github.lablyteam.kitslab.configuration.YamlFile;
+import com.github.lablyteam.kitslab.modules.BinderModule;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.name.Named;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KitsLab extends JavaPlugin {
 
+    @Inject
+    private KitsLabCommand kitsLabCommand;
+
+    @Inject @Named("config")
+    private YamlFile config;
+
+    @Inject @Named("messages")
+    private YamlFile messages;
+
     @Override
     public void onEnable() {
-        // On enable plugin
+        registerModules();
+        registerCommands();
+
     }
 
     @Override
     public void onDisable() {
         // On disable plugin
+    }
+
+    private void registerModules() {
+        BinderModule binderModule = new BinderModule(this);
+        Injector injector = binderModule.createInjector();
+        injector.injectMembers(this);
+    }
+
+    private void registerCommands() {
+        getCommand("kitslab").setExecutor(kitsLabCommand);
+    }
+
+    @Override
+    public YamlFile getConfig() {
+        return config;
+    }
+
+    public YamlFile getMessages() {
+        return messages;
     }
 }

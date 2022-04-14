e95cec90eb3c3f7aa130797491d88ec81e9fd440
jonakls <asmot54@hotmail.com>
2022-04-14 17:55:36 -0500

    build: added Spigot, Guice and BungeeCord chat dependencies to project. Added shadow jar plugin and added task that rename artifact and relocate in bin directory

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,16 +1,37 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.github.lablyteam"
 
 repositories {
     mavenCentral()
-    maven("https://repo.codemc.org/repository/nms/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
 }
 
 dependencies {
     implementation(project(":api"))
-
+    implementation("com.google.inject:guice:4.0")
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-chat:1.8-SNAPSHOT")
+}
+
+tasks {
+    processResources {
+        expand(
+            "version" to version
+        )
+    }
+
+    shadowJar {
+        archiveBaseName.set("KitsLab")
+        destinationDirectory.set(file("$rootDir/bin/"))
+        minimize()
+    }
+
+    clean {
+        delete("$rootDir/bin/")
+    }
 }

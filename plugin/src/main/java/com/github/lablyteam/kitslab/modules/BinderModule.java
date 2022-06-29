47984168d60b068bed290b2934a4b447c2448455
jonakls <asmot54@hotmail.com>
2022-06-28 23:48:38 -0500

    feat: added inject module manager and remove injector method

--- plugin/src/main/java/com/github/lablyteam/kitslab/modules/BinderModule.java
+++ plugin/src/main/java/com/github/lablyteam/kitslab/modules/BinderModule.java
@@ -2,8 +2,6 @@ package com.github.lablyteam.kitslab.modules;
 
 import com.github.lablyteam.kitslab.KitsLab;
 import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 
 public class BinderModule extends AbstractModule {
 
@@ -18,9 +16,6 @@ public class BinderModule extends AbstractModule {
         this.bind(KitsLab.class).toInstance(plugin);
         this.install(new CommandModule());
         this.install(new FilesModule(plugin));
-    }
-
-    public Injector createInjector() {
-        return Guice.createInjector(this);
+        this.install(new ManagerModule());
     }
 }

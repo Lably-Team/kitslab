4820612ab806e1f8d04ee5ad553a422c3e8d20d6
jonakls <asmot54@hotmail.com>
2022-06-28 23:52:13 -0500

    feat(api): changed kits field name

--- api/src/main/java/com/github/lablyteam/kitslab/api/Kits.java
+++ api/src/main/java/com/github/lablyteam/kitslab/api/Kits.java
@@ -4,13 +4,15 @@ import org.bukkit.inventory.ItemStack;
 
 public class Kits {
 
-    private String playerName;
+    private String kitName;
     private String kitPermission;
     private boolean bought;
     private double kitValue;
     private ItemStack[] kitContent;
     private ItemStack[] equipment;
     private long cooldown;
+
+
 
 
 }

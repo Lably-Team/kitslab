a7df050c055016d30aa8fac04864ea9486dfccb8
jonakls <asmot54@hotmail.com>
2022-06-28 23:51:48 -0500

    feat(api): added interface for load managers

---
+++ plugin/src/main/java/com/github/lablyteam/kitslab/managers/KitManager.java
@@ -0,0 +1,16 @@
+package com.github.lablyteam.kitslab.managers;
+
+import com.github.lablyteam.kitslab.api.ManagerLoader;
+
+public class KitManager implements ManagerLoader {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}

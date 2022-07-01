d3adb4286af8fe12614876e632bf760841d85c57
jonakls <asmot54@hotmail.com>
2022-06-30 21:10:59 -0500

    feat(plugin): added builders for kits and items

--- plugin/src/main/java/com/github/lablyteam/kitslab/builder/ItemBuilder.java
+++ plugin/src/main/java/com/github/lablyteam/kitslab/builder/ItemBuilder.java
@@ -1,2 +1,90 @@
-package com.github.lablyteam.kitslab.builder;public class ItemBuilder {
+package com.github.lablyteam.kitslab.builder;
+
+import com.github.lablyteam.kitslab.utils.TextUtils;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.github.lablyteam.kitslab.utils.TextUtils.toLegacy;
+
+public class ItemBuilder {
+
+    private ItemStack item;
+    private final ItemMeta meta;
+
+    private ItemBuilder(Material material) {
+        this.item = new ItemStack(material);
+        this.meta = item.getItemMeta();
+    }
+
+    public static ItemBuilder of(Material material) {
+        return new ItemBuilder(material);
+    }
+
+    public ItemBuilder name(String name) {
+        this.meta.setDisplayName(toLegacy(name));
+        return this;
+    }
+
+    public ItemBuilder lore(String... lore) {
+        List<String> itemLore = new ArrayList<>();
+        for (String line : lore) {
+            itemLore.add(toLegacy(line));
+        }
+        this.meta.setLore(itemLore);
+        return this;
+    }
+
+    public ItemBuilder lore(List<String> lore) {
+        lore.replaceAll(TextUtils::toLegacy);
+        this.meta.setLore(lore);
+        return this;
+    }
+
+    public ItemBuilder amount(int amount) {
+        this.item.setAmount(amount);
+        return this;
+    }
+
+    public ItemBuilder durability(short durability) {
+        this.item.setDurability(durability);
+        return this;
+    }
+
+    public ItemBuilder unbreakable(boolean unbreakable) {
+        this.meta.spigot().setUnbreakable(unbreakable);
+        return this;
+    }
+
+    public ItemBuilder enchant(String enchantmentPath) {
+        String[] enchantmentParts = enchantmentPath.split(":");
+        if (enchantmentParts.length < 2) {
+            throw new IllegalArgumentException("Enchantment path must be in format 'enchantment:level'");
+        }
+
+        if (Integer.parseInt(enchantmentParts[1]) < 1) {
+            throw new IllegalArgumentException("Enchantment level must be greater than 0");
+        }
+
+        if (Enchantment.getByName(enchantmentParts[0]) == null) {
+            throw new IllegalArgumentException("Enchantment '" + enchantmentParts[0] + "' does not exist");
+        }
+
+        this.meta.addEnchant(
+                Enchantment.getByName(enchantmentParts[0].toUpperCase()),
+                Integer.parseInt(enchantmentParts[1]),
+                true
+        );
+        return this;
+    }
+
+
+    public ItemStack build() {
+        this.item.setItemMeta(this.meta);
+        return this.item;
+    }
 }
--- plugin/src/main/java/com/github/lablyteam/kitslab/builder/KitBuilder.java
+++ plugin/src/main/java/com/github/lablyteam/kitslab/builder/KitBuilder.java
@@ -1,2 +1,66 @@
-package com.github.lablyteam.kitslab.builder;public class KitBuilder {
+package com.github.lablyteam.kitslab.builder;
+
+import com.github.lablyteam.kitslab.api.Kits;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+import static com.github.lablyteam.kitslab.utils.TextUtils.toLegacy;
+
+public class KitBuilder {
+
+    private final Kits kit;
+
+    public KitBuilder(String kitId) {
+        this.kit = new Kits(null);
+    }
+
+    public static KitBuilder create(String kitId) {
+        return new KitBuilder(kitId);
+    }
+
+    public KitBuilder name(String kitName) {
+        kit.setKitName(toLegacy(kitName));
+        return this;
+    }
+
+    public KitBuilder setChar(char kitChar) {
+        kit.setKitChar(kitChar);
+        return this;
+    }
+
+    public KitBuilder permission(String kitPermission) {
+        kit.setKitPermission(kitPermission);
+        return this;
+    }
+
+    public KitBuilder bought(boolean bought) {
+        kit.setBought(bought);
+        return this;
+    }
+
+    public KitBuilder value(double kitValue) {
+        kit.setKitValue(kitValue);
+        return this;
+    }
+
+    public KitBuilder content(List<ItemStack> kitContent) {
+        kit.setKitContent(kitContent);
+        return this;
+    }
+
+    public KitBuilder equipment(List<ItemStack> equipment) {
+        kit.setEquipment(equipment);
+        return this;
+    }
+
+    public KitBuilder cooldown(long cooldown) {
+        kit.setCooldown(cooldown);
+        return this;
+    }
+
+    public Kits build() {
+        return kit;
+    }
+
 }

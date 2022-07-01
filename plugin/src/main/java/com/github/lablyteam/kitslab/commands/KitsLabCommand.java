69c40264f260de6ad167ef448f40413a3a85ba26
jonakls <asmot54@hotmail.com>
2022-06-30 21:11:29 -0500

    feat: added test item for player in main command

--- plugin/src/main/java/com/github/lablyteam/kitslab/commands/KitsLabCommand.java
+++ plugin/src/main/java/com/github/lablyteam/kitslab/commands/KitsLabCommand.java
@@ -4,10 +4,15 @@ import com.github.lablyteam.kitslab.KitsLab;
 import com.github.lablyteam.kitslab.configuration.YamlFile;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.logging.Logger;
 
@@ -46,7 +51,29 @@ public class KitsLabCommand implements CommandExecutor {
             }
         }
 
+        Player player = (Player) sender;
+        switch(args[0].toLowerCase()) {
+            case "a":
+                PlayerInventory inventory = player.getInventory();
 
-        return false;
+                ItemStack itemStack = new ItemStack(Material.APPLE);
+                ItemMeta itemMeta = itemStack.getItemMeta();
+                itemMeta.setDisplayName(null);
+                inventory.addItem(itemStack);
+                player.sendMessage("Whosh!!!");
+
+                return true;
+            case "reload":
+                sender.sendMessage(messages.getString("information.plugin-reloaded", true));
+                messages.reload();
+                config.reload();
+                return true;
+            case "give":
+                sender.sendMessage(messages.getString("information.plugin-reloaded", true));
+                return true;
+            default:
+                messages.getStringList("help-messages.console-message").forEach(sender::sendMessage);
+                return true;
+        }
     }
 }

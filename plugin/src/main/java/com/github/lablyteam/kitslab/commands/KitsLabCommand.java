cd2c6cc5ada5d1de546aaa7d3fb68c7481d7e157
jonakls <asmot54@hotmail.com>
2022-07-02 23:41:10 -0500

    feat: added give kits sub command

--- plugin/src/main/java/com/github/lablyteam/kitslab/commands/KitsLabCommand.java
+++ plugin/src/main/java/com/github/lablyteam/kitslab/commands/KitsLabCommand.java
@@ -1,6 +1,7 @@
 package com.github.lablyteam.kitslab.commands;
 
 import com.github.lablyteam.kitslab.KitsLab;
+import com.github.lablyteam.kitslab.api.Kits;
 import com.github.lablyteam.kitslab.configuration.YamlFile;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
@@ -9,7 +10,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -52,6 +52,12 @@ public class KitsLabCommand implements CommandExecutor {
         }
 
         Player player = (Player) sender;
+
+        if (args.length < 1) {
+            player.sendMessage("nao");
+            return true;
+        }
+
         switch(args[0].toLowerCase()) {
             case "a":
                 PlayerInventory inventory = player.getInventory();
@@ -64,11 +70,48 @@ public class KitsLabCommand implements CommandExecutor {
 
                 return true;
             case "reload":
-                sender.sendMessage(messages.getString("information.plugin-reloaded", true));
                 messages.reload();
                 config.reload();
+                plugin.getKitsFilesManager().reloadFiles();
+                plugin.getKitManager().reloadKits();
+
+                sender.sendMessage(messages.getString("information.plugin-reloaded", true));
                 return true;
             case "give":
+
+                if (args.length < 2) {
+                    player.sendMessage(messages.getString("error.give-usage", true));
+                    return true;
+                }
+
+                String kitName = args[1];
+
+
+                Kits kit = plugin.getKitManager().getKit(kitName);
+
+                if (kit == null) {
+                    player.sendMessage(messages.getString("error.kit-not-found", true));
+                    return true;
+                }
+
+                PlayerInventory playerInventory = player.getInventory();
+                int busySlots = 0;
+                for(ItemStack item : playerInventory.getContents()) {
+                    if (item != null) {
+                        busySlots++;
+                    }
+                }
+
+                if (kit.getKitContent().size() <= busySlots) {
+                    player.sendMessage("inventario lleno");
+                    return true;
+
+                }
+
+                for (ItemStack itemStack1 : kit.getKitContent()) {
+                    playerInventory.addItem(itemStack1);
+                }
+
                 sender.sendMessage(messages.getString("information.plugin-reloaded", true));
                 return true;
             default:
